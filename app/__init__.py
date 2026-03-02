27c29173540167ea081a88568937a795db003fa1
Sam Tyler <samtyler905@gmail.com>
2026-03-02 16:36:42 +0000

    User management

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,27 @@
 This module initialises the Flask application, initialises extensions (login manager, etc.)
 and registers application routes.
 """
+import os.path
 
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    instance_path=os.path.join(os.path.dirname(__file__), 'instance'))
 
-#TODO: Initialise database, login manager etc.
+# Configure SQLAlchemy to use SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
+
+# Initialisation
+login_manager = LoginManager(app)
+database = SQLAlchemy(app)
+
+# Redirects users to login view if route requires authentication
+login_manager.login_view = 'login'
 
 # Register application routes and database models
+# Required to avoid circular imports
 from app import routes
 from app import models
--- app/models.py
+++ app/models.py
@@ -1,9 +1,59 @@
 """
 This module will contain the database models, separate to the telemetry data stored on ThingsBoard.
 """
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import database as db
+from app import login_manager as lm
 
-"""
-TODO: Database models (User, Device, etc.)
-      This does not include telemetry data that will be stored on ThingsBoard (e.g. sensor readings, snooze amounts),
-      nor per device settings such as max snoozes (will be stored as shared attributes on ThingsBoard)
-"""
+
+class User(UserMixin, db.Model):
+    """User model for account management."""
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email_address = db.Column(db.String(25), nullable=False, index=True, unique=True)
+    password_hash = db.Column(db.String(64))
+
+    def set_password(self, password):
+        """Hash and store the user's password."""
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password) -> bool:
+        """
+        Checks whether the provided password matches the stored password hash
+        :param password: The password to verify
+        :return: The result of the verification
+        """
+        return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def register(email_address, password) -> 'User':
+        """
+        Creates a new user account and saves it to the database.
+        :param email_address: The user's unique email address
+        :param password: The user's plaintext password
+        :return: The newly created (and stored) User object
+        """
+
+        # Fill out attributes
+        user = User()
+        user.email_address = email_address
+        user.set_password(password)
+
+        # Save to DB
+        db.session.add(user)
+        db.session.commit()
+
+        return user
+
+    @staticmethod
+    @lm.user_loader
+    def load_user(user_id: int):
+        return User.query.get(int(user_id))
+
+
+
+# TODO: Other database models (Device, etc.)
+#       This does not include telemetry data that will be stored on ThingsBoard (e.g. sensor readings, data collected from
+#       the alarm), nor per device settings such as max snoozes (will be stored as shared attributes on ThingsBoard)
