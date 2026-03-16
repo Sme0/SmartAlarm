9b51ac1649908d3db9e84e1bb20ba1b392eb11d2
lwatgit <lwatkins1206@gmail.com>
2026-03-16 10:51:20 +0000

    Add files via upload

--- alarm/memory game/memoryGame.py
+++ alarm/memory game/memoryGame.py
@@ -1,19 +1,44 @@
 import random as rand
 from joystickDirections import *
+from alarmClockDisplay import *
 import grovepi
 
-def simonSays(xpin, ypin):
-    #generates the pattern
+def generatePattern():
     directions = ["UP", "DOWN", "LEFT", "RIGHT"]
     puzzle_length = 5 #maybe change to variable that the user can edit
     instructions = []
     for i in range(0, puzzle_length):
         x = rand.randint(0,3)
         instructions.append(directions[x])
+    return instructions
+
+def printInstructions(instructions):
+    d = Display()
+    
+    d.set_text("Memory game: Copy the\norder with the joystick.")
+
+    # create new memory game
+    md = MemoryDisplay(instructions)
+    
+    # format directions for display
+    directions = md.format_directions()
+    
+    # iterate through directions
+    for i in directions:
+        d.set_text(i)
+        sleep(1)
+    
+    d.set_text(" ")
+
+
+def simonSays(xpin, ypin):
+    #generates the pattern
+    instructions = generatePattern()
 
     #display instructions
     #change to call proper lcd display
     print(instructions)
+    printInstructions(instructions)
     
     #takes user input, pulls from joystickDirections
     direction_values = readDirections(xpin,ypin)
