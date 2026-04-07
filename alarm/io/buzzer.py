7b7189f369f5815d293bbde666f6369a9813c6fb
Ben Parker <benparker06@protonmail.com>
2026-04-07 15:47:44 +0100

    Created buzzer.py to control alarm sound output

---
+++ alarm/io/buzzer.py
@@ -0,0 +1,62 @@
+import threading
+from time import sleep
+
+import grovepi
+
+
+class Buzzer:
+    """
+    Class to control raspberry pi buzzer.
+    """
+
+    def __init__(self, volume: int = 3) -> None:
+        self.active = False
+        self.volume = volume
+        self.pin = 5
+
+        grovepi.pinMode(self.pin, "OUTPUT")
+        grovepi.analogWrote(self.pin, 0)
+
+    def play_single_sound(self, duration: float = 0.5) -> None:
+        """
+        Play a constant sound for a given duration.
+        """
+        grovepi.analogWrite(self.pin, 5 * self.volume)
+        sleep(duration)
+        grovepi.analogWrite(self.pin, 0)
+
+    def alarm_sound(self) -> None:
+        """
+        Internal method defining the alarm tune
+        """
+        while self.active:
+            for _ in range(4):
+                grovepi.analogWrite(self.pin, 10 * self.volume)
+                sleep(0.05)
+                grovepi.analogWrite(self.pin, 1 * self.volume)
+                sleep(0.1)
+
+            grovepi.analogWrite(self.pin, 0)
+            sleep(0.4)
+
+        grovepi.analogWrite(self.pin, 0)
+
+    def play_alarm_sound(self) -> None:
+        """
+        Starts a thread to play the alarm sound
+        """
+        self.active = True
+        buzzer_thread = threading.Thread(target=self.alarm_sound)
+        buzzer_thread.start()
+
+    def stop_alarm_sound(self) -> None:
+        """
+        Stops the alarm if it's playing
+        """
+        self.active = False
+
+    def set_alarm_volume(self, volume: int) -> None:
+        """
+        Sets the loudness of any noise from the buzzer from 0-25
+        """
+        self.volume = max(0, min(volume, 25))
