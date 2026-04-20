e100d55bb8dd97e94169bad861ac17a3de2d1927
Sam Tyler <samtyler905@gmail.com>
2026-04-20 01:01:49 +0100

    Expanded ThingsBoardClient

--- alarm/thingsboard_client.py
+++ alarm/thingsboard_client.py
@@ -1,3 +1,5 @@
+import json
+
 from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
 import os
@@ -10,33 +12,53 @@ class ThingsBoardClient:
         self.host = host or os.getenv("THINGSBOARD_HOST")
         self.access_token = token or os.getenv("THINGSBOARD_ACCESS_TOKEN")
 
-    def _on_connect(client, userdata, flags, rc, *extra_params):
+    def _on_connect(self, client, userdata, flags, rc, *extra_params):
         """
         Called after the client attempts to connect to the MQTT broker.
         rc (result code) = 0 means a successful connection.
-        This is a good place to log or print a message indicating the connection status.
         """
         print('Connected with result code ' + str(rc))
 
-    def _on_publish(client, userdata, result):
+    def _on_publish(self, client, userdata, result):
         """
         Called whenever a message is successfully published to the MQTT broker.
-        Prints 'Success' to confirm the publish event, helping you verify
+        Prints to confirm the publish event, helping verify
         that data actually reached the server.
         """
         print("Published to MQTT broker")
+
+    def _on_message(self, client, userdata, msg):
+        print("Payload: " + str(msg.payload))
+        data = json.loads(msg.payload)
+        #TODO: Send data somewhere
 
     def connect(self):
         self.client.username_pw_set(self.access_token)
         self.client.on_connect = self._on_connect
         self.client.on_publish = self._on_publish
-        #TODO: self.client.on_message =
+        self.client.on_message = self._on_message
 
-        #TODO: self.client.subscribe('')
+        self.client.subscribe('v1/devices/me/attributes')
+        self.client.subscribe('v1/devices/me/rpc/request/+')
+
+        self.client.connect(self.host, 1883, 60)
         self.client.loop_start()
 
     def disconnect(self):
         self.client.loop_stop()
+        self.client.disconnect()
+
+    def post(self, data: dict):
+        self.client.publish('v1/devices/me/telemetry', json.dumps(data), qos=1)
+
+    def request_shared_attributes(self, keys: list):
+        payload = {"sharedKeys": ",".join(keys)}
+        self.client.publish(
+            'v1/devices/me/attributes/request/1',
+            json.dumps(payload),
+            qos=1
+        )
+
 
 
 
